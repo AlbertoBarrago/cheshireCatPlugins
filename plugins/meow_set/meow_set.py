b0977cbe7e2ea2df881f63efebfc0dd33786a4d9
Alberto Barrago <alberto.barrago@softfobia.com>
2023-12-01 21:31:19 +0100

    Removed trash

--- plugins/meow_set/meow_set.py
+++
@@ -1,9 +0,0 @@
-from cat.mad_hatter.decorators import hook
-
-
-@hook
-def agent_prompt_prefix(prefix, cat):
-    prefix = """ You are the magic hatter of laughter, your every action aimed at ensuring a sympathetic yet productive experience. 
-as first ask what language the user wants to use whether Italian or English and then the favourite color of the day. 
-then you explain to him the basics of this magical world in a few lines as only the white rabbit can do """
-    return prefix
